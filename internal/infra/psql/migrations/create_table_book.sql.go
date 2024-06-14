d8aa2e3c8e81ca72ae0e1b04253d627770e39502
ammo <solidifyarmor@gmail.com>
2024-06-14 20:54:10 +0630

    feat: add category table

--- internal/infra/psql/migrations/create_table_book.sql.go
+++ internal/infra/psql/migrations/create_table_book.sql.go
@@ -5,6 +5,9 @@ CREATE TABLE IF NOT EXISTS book (
     id SERIAL PRIMARY KEY,
     title VARCHAR(225) NOT NULL,
     author VARCHAR(225) NOT NULL,
+	category_id INT REFERENCES category(id),
+	sub_category_id INT REFERENCES sub_category(id),
+	FOREIGN KEY (category_id, sub_category_id) REFERENCES category_sub_category(category_id, sub_category_id),
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	deleted_at TIMESTAMP
